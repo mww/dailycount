42abeed997259c75df30925b3d2e6106821846f6
Mark Weaver <weaver.mark@gmail.com>
2011-01-02 22:04:32 -0800

    Remove the CountItemHandler.get() method since post() is now used and get contains almost identical code.

--- dailycount.py
+++ dailycount.py
@@ -232,32 +232,6 @@ class CountItemHandler(BaseHandler):
         (item_type.name, comment, num))
     self.write('%d' % num)
 
-  @login_required
-  def get(self):
-    type_name = self.get_argument('type', None)
-
-    if type_name is None:
-      logging.error('type pass to CountItemHandler is None')
-      # TODO(mww): Find a more specific 500 to serve
-      raise tornado.web.HTTPError(500)
-      return
-
-    q = db.Query(ItemType).filter('name = ', type_name)
-    q.filter('active = ', True)
-    item_type = q.get()
-    if item_type is None:
-      logging.error('No active type for name "%s" could be found.' % type_name)
-      # TODO(mww): Find a more specific 500 to serve
-      raise tornado.web.HTTPError(500)
-      return
-
-    q = db.Query(CountedItem)
-    q.filter('user =', self.get_current_user())
-    q.filter('item_type =', item_type)
-    q.filter('date >', datetime.date.today())
-    num = q.count()
-    self.write('%d' % num)
-
 
 class CreateItemTypeHandler(BaseHandler):
   @administrator
